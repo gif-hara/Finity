63614fbf4e23454199e48b7f1e34eb788a4fc613
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2015-08-13 23:37:42 +0900

    魚移動処理の実装中

---
+++ trunk/Assets/Scripts/Game/Component/AutoMoveTo.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+
+/// <summary>
+/// .
+/// </summary>
+public class AutoMoveTo : MonoBehaviour
+{
+	[SerializeField]
+	private Vector3 range;
+
+	[SerializeField]
+	private float minDelay;
+
+	[SerializeField]
+	private float maxDelay;
+
+	[SerializeField]
+	private float minMoveTime;
+
+	[SerializeField]
+	private float maxMoveTime;
+
+	private List<MoveTo> moveToList;
+
+	void Start()
+	{
+		BroadcastMessage("AttachOnCompleteTarget", gameObject);
+		this.moveToList = GetComponentsInChildren<MoveTo>();
+		foreach(var moveTo in moveToList)
+		{
+//			moveTo.Move(
+		}
+	}
+
+	void OnDrawGizmosSelected()
+	{
+		Gizmos.DrawWireCube(transform.position, range);
+	}
+
+	void OnCompleteMove(MoveTo completeMoveTo)
+	{
+		Debug.Log(completeMoveTo.gameObject.name);
+	}
+
+	private void Move(MoveTo moveTo)
+	{
+		var target = this.transform.position;
+		target += new Vector3(
+			Random.Range(-range.x, range.x),
+			Random.Range(-range.y, range.y),
+			Random.Range(-range.z, range.z)
+			);
+		var delay = Random.Range(minDelay, maxDelay);
+
+		moveTo.Move(target, delay,
+	}
+}
---
+++ trunk/Assets/Scripts/Game/Component/MoveTo.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+/// <summary>
+/// .
+/// </summary>
+public class MoveTo : MonoBehaviour
+{
+	[SerializeField]
+	private Vector3 target;
+
+	[SerializeField]
+	private float delay;
+	
+	[SerializeField]
+	private float moveTime;
+
+	[SerializeField]
+	private float lookTime;
+
+	[SerializeField]
+	private string easeType;
+
+	private GameObject onCompleteTarget;
+
+	// Use this for initialization	
+	void Start ()
+	{
+		Move(target, delay, moveTime, lookTime, easeType);
+	}
+
+	void AttachOnCompleteTarget(GameObject onCompleteTarget)
+	{
+		this.onCompleteTarget = onCompleteTarget;
+	}
+
+	public void Move(Vector3 target, float delay, float moveTime, float lookTime, string easeType)
+	{
+		var hash = iTween.Hash(
+			"position", target,
+			"time", moveTime,
+			"delay", delay,
+			"looktarget", target,
+			"looktime", lookTime,
+			"easetype", easeType,
+			"oncompletetarget", onCompleteTarget,
+			"oncomplete", "OnCompleteMove",
+			"oncompleteparams", this
+			);
+		iTween.MoveTo(this.gameObject, hash);
+	}
+}
